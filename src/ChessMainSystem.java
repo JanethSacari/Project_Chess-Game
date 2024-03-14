71c7de4e7577b049ffa2fe7c3f3a41919beb2508
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-03-14 00:13:34 -0300

    Position class created.

--- src/ChessMainSystem.java
+++ src/ChessMainSystem.java
@@ -1,10 +1,15 @@
 package src;
 
+import boardgame.Position;
+
 public class ChessMainSystem {
 
     public static void main(String ... args){
 
         System.out.println("Game started!");
+
+        Position position = new Position(2, 3);
+        System.out.println(position);
 
     }
 
