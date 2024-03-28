62ff6740470f9bddc117057dbb219cd10d5b76ef
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-03-27 22:12:34 -0300

    Project: Chess Game - Switching player each turn

--- src/game/ChessMainSystem.java
+++ src/game/ChessMainSystem.java
@@ -18,7 +18,7 @@ public class ChessMainSystem {
         while (true) {
             try {
                 UserInterface.clearScreen();
-                UserInterface.printBoard(chessMatch.getPieces());
+                UserInterface.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UserInterface.readChessPosition(scanner);
