c3e3fb1bb7860104e7a5c9b3dbb78d1647e32aba
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-04-03 22:40:17 -0300

    Project: Chess Game - Implementing knight moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,10 +3,7 @@ package src.chess;
 import src.boardgame.Board;
 import src.boardgame.Piece;
 import src.boardgame.Position;
-import src.chess.pieces.Bishop;
-import src.chess.pieces.King;
-import src.chess.pieces.Pawn;
-import src.chess.pieces.Rook;
+import src.chess.pieces.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -189,9 +186,11 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.BLUE));
+        placeNewPiece('b', 1, new Knight(board, Color.BLUE));
         placeNewPiece('c', 1, new Bishop(board, Color.BLUE));
         placeNewPiece('e', 1, new King(board, Color.BLUE));
         placeNewPiece('f', 1, new Bishop(board, Color.BLUE));
+        placeNewPiece('g', 1, new Knight(board, Color.BLUE));
         placeNewPiece('h', 1, new Rook(board, Color.BLUE));
         placeNewPiece('a', 2, new Pawn(board, Color.BLUE));
         placeNewPiece('b', 2, new Pawn(board, Color.BLUE));
@@ -203,9 +202,11 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.BLUE));
 
         placeNewPiece('a', 8, new Rook(board, Color.RED));
+        placeNewPiece('b', 8, new Knight(board, Color.RED));
         placeNewPiece('c', 8, new Bishop(board, Color.RED));
         placeNewPiece('e', 8, new King(board, Color.RED));
         placeNewPiece('f', 8, new Bishop(board, Color.RED));
+        placeNewPiece('g', 8, new Knight(board, Color.RED));
         placeNewPiece('h', 8, new Rook(board, Color.RED));
         placeNewPiece('a', 7, new Pawn(board, Color.RED));
         placeNewPiece('b', 7, new Pawn(board, Color.RED));
