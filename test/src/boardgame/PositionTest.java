c0fa371eac28d1d01c3a7b279198ef7c35ae5ab6
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-10 22:18:40 -0300

    Project Chess Game: Adding Position Test

---
+++ test/src/boardgame/PositionTest.java
@@ -0,0 +1,38 @@
+package test.src.boardgame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRow() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getColumn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setColumn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setValues() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
