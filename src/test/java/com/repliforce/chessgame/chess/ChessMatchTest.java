f606daf76a8605316c03393cde0184acefc4fff2
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-18 21:12:36 -0300

    Project Chess Game: Add ChessMatchTest.

---
+++ src/test/java/com/repliforce/chessgame/chess/ChessMatchTest.java
@@ -0,0 +1,58 @@
+package com.repliforce.chessgame.chess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessMatchTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getTurn() {
+    }
+
+    @Test
+    void getCurrentPlayer() {
+    }
+
+    @Test
+    void getCheck() {
+    }
+
+    @Test
+    void getCheckMate() {
+    }
+
+    @Test
+    void getPromoted() {
+    }
+
+    @Test
+    void getEnPassantVulnerable() {
+    }
+
+    @Test
+    void getPieces() {
+    }
+
+    @Test
+    void possibleMoves() {
+    }
+
+    @Test
+    void performChessMove() {
+    }
+
+    @Test
+    void replacePromotedPiece() {
+    }
+}
