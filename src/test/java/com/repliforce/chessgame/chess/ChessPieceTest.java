dafe6d59d7790c210b9e1545f83855d68c6cc2f9
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-19 23:47:33 -0300

    Project Chess Game: Add ChessPieceTest.

---
+++ src/test/java/com/repliforce/chessgame/chess/ChessPieceTest.java
@@ -0,0 +1,54 @@
+package com.repliforce.chessgame.chess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessPieceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void possibleMove() {
+    }
+
+    @Test
+    void isThereAnyPossibleMove() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void getMoveCount() {
+    }
+
+    @Test
+    void increaseMoveCount() {
+    }
+
+    @Test
+    void decreaseMoveCount() {
+    }
+
+    @Test
+    void getChessPosition() {
+    }
+
+    @Test
+    void isThereOpponentPiece() {
+    }
+}
