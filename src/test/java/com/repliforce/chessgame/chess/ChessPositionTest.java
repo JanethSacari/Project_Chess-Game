21bb4d141ab749692120943c1d7c33e5c9ed7af0
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-20 22:28:52 -0300

    Project Chess Game: Add ChessPositionTest

---
+++ src/test/java/com/repliforce/chessgame/chess/ChessPositionTest.java
@@ -0,0 +1,38 @@
+package com.repliforce.chessgame.chess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessPositionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getColumn() {
+    }
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void toPosition() {
+    }
+
+    @Test
+    void fromPosition() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
