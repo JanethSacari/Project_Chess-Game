013a6e5f3ea691c01284a455ba8dfee7e63c510b
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-23 20:56:39 -0300

    Project Chess Game: Add KnightTest

---
+++ src/test/java/com/repliforce/chessgame/chess/pieces/KnightTest.java
@@ -0,0 +1,62 @@
+package com.repliforce.chessgame.chess.pieces;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void getMoveCount() {
+    }
+
+    @Test
+    void increaseMoveCount() {
+    }
+
+    @Test
+    void decreaseMoveCount() {
+    }
+
+    @Test
+    void getChessPosition() {
+    }
+
+    @Test
+    void isThereOpponentPiece() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void possibleMove() {
+    }
+
+    @Test
+    void isThereAnyPossibleMove() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void possibleMoves() {
+    }
+}
