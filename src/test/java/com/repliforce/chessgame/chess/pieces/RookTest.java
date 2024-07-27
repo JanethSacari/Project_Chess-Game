1208178b4e7747451c7863f3422c015a3e25a063
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-27 16:16:47 -0300

    Project Chess Game: Add RookTest adjustment

--- src/test/java/com/repliforce/chessgame/chess/pieces/RookTest.java
+++ src/test/java/com/repliforce/chessgame/chess/pieces/RookTest.java
@@ -1,4 +1,62 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.repliforce.chessgame.chess.pieces;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
 class RookTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void getMoveCount() {
+    }
+
+    @Test
+    void increaseMoveCount() {
+    }
+
+    @Test
+    void decreaseMoveCount() {
+    }
+
+    @Test
+    void getChessPosition() {
+    }
+
+    @Test
+    void isThereOpponentPiece() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+
+    @Test
+    void possibleMove() {
+    }
+
+    @Test
+    void isThereAnyPossibleMove() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void possibleMoves() {
+    }
 }
