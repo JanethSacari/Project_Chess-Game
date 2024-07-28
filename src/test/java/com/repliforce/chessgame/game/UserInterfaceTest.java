c4af4d1fdc0ee73d881f8bb0d9843306407f90b7
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-07-28 20:49:23 -0300

    Project Chess Game: User Interface Test

---
+++ src/test/java/com/repliforce/chessgame/game/UserInterfaceTest.java
@@ -0,0 +1,38 @@
+package com.repliforce.chessgame.game;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInterfaceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void clearScreen() {
+    }
+
+    @Test
+    void readChessPosition() {
+    }
+
+    @Test
+    void printMatch() {
+    }
+
+    @Test
+    void printBoard() {
+    }
+
+    @Test
+    void testPrintBoard() {
+    }
+}
