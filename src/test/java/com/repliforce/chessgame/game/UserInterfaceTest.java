5d75247d181504eee5d892a67e6e5de87f99d020
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-07 22:23:37 -0300

    Project Chess Game: UserInterfaceTest method readChessPosition

--- src/test/java/com/repliforce/chessgame/game/UserInterfaceTest.java
+++ src/test/java/com/repliforce/chessgame/game/UserInterfaceTest.java
@@ -1,8 +1,11 @@
 package com.repliforce.chessgame.game;
 
+import com.repliforce.chessgame.chess.ChessPosition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +25,11 @@ class UserInterfaceTest {
 
     @Test
     void readChessPosition() {
+        Scanner scanner = new Scanner("e2");
+        ChessPosition position = UserInterface.readChessPosition(scanner);
+        
+        assertEquals('e', position.getColumn());
+        assertEquals(2, position.getRow());
     }
 
     @Test
