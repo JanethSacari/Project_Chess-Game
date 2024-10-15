d48a94bf631f6fec3b56c51b0464431d8eb58371
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-14 23:14:02 -0300

    Project Chess Game - PositionTest implemented.

--- src/test/java/com/repliforce/chessgame/boardgame/PositionTest.java
+++ src/test/java/com/repliforce/chessgame/boardgame/PositionTest.java
@@ -1,41 +1,56 @@
 package com.repliforce.chessgame.boardgame;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PositionTest {
+public class PositionTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void testPositionConstructor() {
+        Position position = new Position(3, 5);
+        assertEquals(3, position.getRow());
+        assertEquals(5, position.getColumn());
     }
 
     @Test
-    void getRow() {
+    public void testGetRow() {
+        Position position = new Position(2, 4);
+        assertEquals(2, position.getRow());
     }
 
     @Test
-    void setRow() {
+    public void testSetRow() {
+        Position position = new Position(2, 4);
+        position.setRow(6);
+        assertEquals(6, position.getRow());
     }
 
     @Test
-    void getColumn() {
+    public void testGetColumn() {
+        Position position = new Position(3, 7);
+        assertEquals(7, position.getColumn());
     }
 
     @Test
-    void setColumn() {
+    public void testSetColumn() {
+        Position position = new Position(3, 7);
+        position.setColumn(9); // Define uma nova coluna
+        assertEquals(9, position.getColumn());
     }
 
     @Test
-    void setValues() {
+    public void testSetValues() {
+        Position position = new Position(1, 2);
+        position.setValues(5, 8);
+        assertEquals(5, position.getRow());
+        assertEquals(8, position.getColumn());
     }
 
     @Test
-    void testToString() {
+    public void testToString() {
+        Position position = new Position(3, 6);
+        assertEquals("3, 6", position.toString());
     }
+
 }
