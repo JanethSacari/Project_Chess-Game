e2c3813c658a3c20163f151e87a8241184a4e761
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-21 23:31:39 -0300

    Project Chess Game - Implementing ChessMatchTest

--- src/test/java/com/repliforce/chessgame/chess/ChessMatchTest.java
+++ src/test/java/com/repliforce/chessgame/chess/ChessMatchTest.java
@@ -1,58 +1,39 @@
 package com.repliforce.chessgame.chess;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.repliforce.chessgame.chess.pieces.King;
+import com.repliforce.chessgame.chess.pieces.Pawn;
+import com.repliforce.chessgame.chess.pieces.Queen;
+import com.repliforce.chessgame.chess.pieces.Rook;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChessMatchTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Test
+    public void testNextTurn() {
+        ChessMatch match = new ChessMatch();
 
-    @AfterEach
-    void tearDown() {
+        assertEquals(1, match.getTurn());
+        assertEquals(Color.BLUE, match.getCurrentPlayer());
+
+        ChessPosition source = new ChessPosition('a', 2);
+        ChessPosition target = new ChessPosition('a', 3);
+        match.performChessMove(source, target);
+
+        assertEquals(2, match.getTurn());
+        assertEquals(Color.RED, match.getCurrentPlayer());
     }
 
     @Test
-    void getTurn() {
+    public void testInvalidMove() {
+        ChessMatch match = new ChessMatch();
+        ChessPosition source = new ChessPosition('a', 1);
+        ChessPosition target = new ChessPosition('a', 3);
+
+        assertThrows(ChessException.class, () -> {
+            match.performChessMove(source, target);
+        });
     }
 
-    @Test
-    void getCurrentPlayer() {
-    }
-
-    @Test
-    void getCheck() {
-    }
-
-    @Test
-    void getCheckMate() {
-    }
-
-    @Test
-    void getPromoted() {
-    }
-
-    @Test
-    void getEnPassantVulnerable() {
-    }
-
-    @Test
-    void getPieces() {
-    }
-
-    @Test
-    void possibleMoves() {
-    }
-
-    @Test
-    void performChessMove() {
-    }
-
-    @Test
-    void replacePromotedPiece() {
-    }
 }
