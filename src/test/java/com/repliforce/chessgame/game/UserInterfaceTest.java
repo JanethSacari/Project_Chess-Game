96619278fb6634fd47e04b0dd0b6e3d43fc02dc2
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-23 23:33:43 -0300

    Project Chess Game - Implementing UserInterfaceTest

--- src/test/java/com/repliforce/chessgame/game/UserInterfaceTest.java
+++ src/test/java/com/repliforce/chessgame/game/UserInterfaceTest.java
@@ -1,56 +1,15 @@
 package com.repliforce.chessgame.game;
 
-import com.repliforce.chessgame.chess.ChessMatch;
 import com.repliforce.chessgame.chess.ChessPiece;
 import com.repliforce.chessgame.chess.ChessPosition;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import java.util.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class UserInterfaceTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void clearScreen() {
-    }
-
-    @Test
-    void readChessPosition() {
-        Scanner scanner = new Scanner("e2");
-        ChessPosition position = UserInterface.readChessPosition(scanner);
-        
-        assertEquals('e', position.getColumn());
-        assertEquals(2, position.getRow());
-    }
-
-    @Test
-    public void testReadChessPositionInvalid() {
-        Scanner scanner = new Scanner("z9");
-
-        assertThrows(InputMismatchException.class, () -> {
-            UserInterface.readChessPosition(scanner);
-        });
-    }
-
-    @Test
-    public void testClearScreen() throws Exception {
-        String result = tapSystemOut(() -> {
-            UserInterface.clearScreen();
-        });
-        assertTrue(result.contains("\033[H\033[2J"));
-    }
+public class UserInterfaceTest {
 
     @Test
     public void testReadChessPosition_ValidInput() {
@@ -60,33 +19,28 @@ class UserInterfaceTest {
         assertEquals(2, position.getRow());
     }
 
-    @Test
+    @Test(expected = InputMismatchException.class)
     public void testReadChessPosition_InvalidInput() {
         Scanner scanner = new Scanner("z9\n");
-        assertThrows(InputMismatchException.class, () -> {
-            UserInterface.readChessPosition(scanner);
-        });
+        UserInterface.readChessPosition(scanner);
     }
 
     @Test
-    public void testPrintBoard() throws Exception {
-        ChessPiece[][] pieces = new ChessPiece[8][8];
-        String result = tapSystemOut(() -> {
-            UserInterface.printBoard(pieces);
-        });
-        assertTrue(result.contains("  a b c d e f g h"));
+    public void testPrintBoard_EmptyBoard() {
+        ChessPiece[][] emptyBoard = new ChessPiece[8][8];
+        UserInterface.printBoard(emptyBoard);
     }
 
     @Test
-    public void testPrintMatch() throws Exception {
-        ChessMatch chessMatch = new ChessMatch();
-        List<ChessPiece> capturedPieces = new ArrayList<>();
+    public void testPrintBoardWithPossibleMoves_EmptyBoard() {
+        ChessPiece[][] emptyBoard = new ChessPiece[8][8];
+        boolean[][] possibleMoves = new boolean[8][8];
+        possibleMoves[2][2] = true;
+        UserInterface.printBoard(emptyBoard, possibleMoves);
+    }
 
-        String result = tapSystemOut(() -> {
-            UserInterface.printMatch(chessMatch, capturedPieces);
-        });
-
-        assertTrue(result.contains("Turn:"));
-        assertTrue(result.contains("Waiting player:"));
+    @Test
+    public void testClearScreen() {
+        UserInterface.clearScreen();
     }
 }
