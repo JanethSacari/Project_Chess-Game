85906d637d4ee9f6b673630eb1063c6fd902e82a
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-26 23:28:04 -0300

    Project Chess Game - Implementing BishopTest

--- src/test/java/com/repliforce/chessgame/chess/pieces/BishopTest.java
+++ src/test/java/com/repliforce/chessgame/chess/pieces/BishopTest.java
@@ -1,62 +1,97 @@
 package com.repliforce.chessgame.chess.pieces;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.repliforce.chessgame.boardgame.Board;
+import com.repliforce.chessgame.boardgame.Position;
+import com.repliforce.chessgame.chess.ChessPiece;
+import com.repliforce.chessgame.chess.Color;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BishopTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Test
+    void testPossibleMoves_BishopInCenterOfEmptyBoard() {
+        Board board = new Board(8, 8);
+        Bishop bishop = new Bishop(board, Color.WHITE);
+        Position bishopPosition = new Position(4, 4);
+        board.placePiece(bishop, bishopPosition);
 
-    @AfterEach
-    void tearDown() {
+        boolean[][] possibleMoves = bishop.possibleMoves();
+
+        assertTrue(possibleMoves[3][3]); // NW diagonal
+        assertTrue(possibleMoves[2][2]);
+        assertTrue(possibleMoves[1][1]);
+        assertTrue(possibleMoves[0][0]);
+
+        assertTrue(possibleMoves[3][5]); // NE diagonal
+        assertTrue(possibleMoves[2][6]);
+        assertTrue(possibleMoves[1][7]);
+
+        assertTrue(possibleMoves[5][5]); // SE diagonal
+        assertTrue(possibleMoves[6][6]);
+        assertTrue(possibleMoves[7][7]);
+
+        assertTrue(possibleMoves[5][3]); // SW diagonal
+        assertTrue(possibleMoves[6][2]);
+        assertTrue(possibleMoves[7][1]);
     }
 
     @Test
-    void getColor() {
+    void testPossibleMoves_BishopBlockedBySameColorPieces() {
+        Board board = new Board(8, 8);
+        Bishop bishop = new Bishop(board, Color.WHITE);
+        Position bishopPosition = new Position(4, 4);
+        board.placePiece(bishop, bishopPosition);
+
+        ChessPiece blockingPiece1 = new Bishop(board, Color.WHITE);
+        ChessPiece blockingPiece2 = new Bishop(board, Color.WHITE);
+        board.placePiece(blockingPiece1, new Position(3, 3)); // NW diagonal
+        board.placePiece(blockingPiece2, new Position(5, 5)); // SE diagonal
+
+        // Act
+        boolean[][] possibleMoves = bishop.possibleMoves();
+
+        // Assert
+        // NW diagonal should be blocked by (3,3)
+        assertFalse(possibleMoves[3][3]);
+        assertFalse(possibleMoves[2][2]);
+
+        // NE diagonal should be unblocked
+        assertTrue(possibleMoves[3][5]);
+        assertTrue(possibleMoves[2][6]);
+
+        // SE diagonal should be blocked by (5,5)
+        assertFalse(possibleMoves[5][5]);
+        assertFalse(possibleMoves[6][6]);
+
+        // SW diagonal should be unblocked
+        assertTrue(possibleMoves[5][3]);
+        assertTrue(possibleMoves[6][2]);
     }
 
     @Test
-    void getMoveCount() {
+    void testPossibleMoves_BishopCapturingOpponentPieces() {
+        Board board = new Board(8, 8);
+        Bishop bishop = new Bishop(board, Color.WHITE);
+        Position bishopPosition = new Position(4, 4);
+        board.placePiece(bishop, bishopPosition);
+
+        ChessPiece opponentPiece1 = new Bishop(board, Color.RED);
+        ChessPiece opponentPiece2 = new Bishop(board, Color.RED);
+        board.placePiece(opponentPiece1, new Position(3, 3)); // NW diagonal
+        board.placePiece(opponentPiece2, new Position(5, 5)); // SE diagonal
+
+        // Act
+        boolean[][] possibleMoves = bishop.possibleMoves();
+
+        assertTrue(possibleMoves[3][3]);
+
+        assertTrue(possibleMoves[5][5]);
+
+        assertTrue(possibleMoves[3][5]);
+        assertTrue(possibleMoves[2][6]);
+        assertTrue(possibleMoves[5][3]);
+        assertTrue(possibleMoves[6][2]);
     }
 
-    @Test
-    void increaseMoveCount() {
-    }
-
-    @Test
-    void decreaseMoveCount() {
-    }
-
-    @Test
-    void getChessPosition() {
-    }
-
-    @Test
-    void isThereOpponentPiece() {
-    }
-
-    @Test
-    void getBoard() {
-    }
-
-    @Test
-    void possibleMove() {
-    }
-
-    @Test
-    void isThereAnyPossibleMove() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void possibleMoves() {
-    }
 }
