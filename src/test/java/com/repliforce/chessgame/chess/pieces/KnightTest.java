55d7886a9ab9ccadd06fce05abcd3ffded439b8d
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-28 22:46:38 -0300

    Project Chess Game - Implementing KnightTest

--- src/test/java/com/repliforce/chessgame/chess/pieces/KnightTest.java
+++ src/test/java/com/repliforce/chessgame/chess/pieces/KnightTest.java
@@ -1,62 +1,115 @@
 package com.repliforce.chessgame.chess.pieces;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.repliforce.chessgame.boardgame.Board;
+import com.repliforce.chessgame.boardgame.Position;
+import com.repliforce.chessgame.chess.Color;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KnightTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Test
+    void testPossibleMoves_KnightInCenterOfEmptyBoard() {
+        Board board = new Board(8, 8);
+        Knight knight = new Knight(board, Color.WHITE);
+        Position knightPosition = new Position(4, 4);
+        board.placePiece(knight, knightPosition);
 
-    @AfterEach
-    void tearDown() {
+        boolean[][] possibleMoves = knight.possibleMoves();
+
+        assertTrue(possibleMoves[3][2]); // 1 up, 2 left
+        assertTrue(possibleMoves[2][3]); // 2 up, 1 left
+        assertTrue(possibleMoves[2][5]); // 2 up, 1 right
+        assertTrue(possibleMoves[3][6]); // 1 up, 2 right
+        assertTrue(possibleMoves[5][6]); // 1 down, 2 right
+        assertTrue(possibleMoves[6][5]); // 2 down, 1 right
+        assertTrue(possibleMoves[6][3]); // 2 down, 1 left
+        assertTrue(possibleMoves[5][2]); // 1 down, 2 left
     }
 
     @Test
-    void getColor() {
+    void testPossibleMoves_KnightBlockedBySameColorPieces() {
+        Board board = new Board(8, 8);
+        Knight knight = new Knight(board, Color.WHITE);
+        Position knightPosition = new Position(4, 4);
+        board.placePiece(knight, knightPosition);
+
+        board.placePiece(new Rook(board, Color.WHITE), new Position(3, 2)); // 1 up, 2 left
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 3)); // 2 up, 1 left
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 5)); // 2 up, 1 right
+        board.placePiece(new Rook(board, Color.WHITE), new Position(3, 6)); // 1 up, 2 right
+        board.placePiece(new Rook(board, Color.WHITE), new Position(5, 6)); // 1 down, 2 right
+        board.placePiece(new Rook(board, Color.WHITE), new Position(6, 5)); // 2 down, 1 right
+        board.placePiece(new Rook(board, Color.WHITE), new Position(6, 3)); // 2 down, 1 left
+        board.placePiece(new Rook(board, Color.WHITE), new Position(5, 2)); // 1 down, 2 left
+
+        boolean[][] possibleMoves = knight.possibleMoves();
+
+        assertFalse(possibleMoves[3][2]); // 1 up, 2 left
+        assertFalse(possibleMoves[2][3]); // 2 up, 1 left
+        assertFalse(possibleMoves[2][5]); // 2 up, 1 right
+        assertFalse(possibleMoves[3][6]); // 1 up, 2 right
+        assertFalse(possibleMoves[5][6]); // 1 down, 2 right
+        assertFalse(possibleMoves[6][5]); // 2 down, 1 right
+        assertFalse(possibleMoves[6][3]); // 2 down, 1 left
+        assertFalse(possibleMoves[5][2]); // 1 down, 2 left
     }
 
     @Test
-    void getMoveCount() {
+    void testPossibleMoves_KnightCapturingOpponentPieces() {
+        Board board = new Board(8, 8);
+        Knight knight = new Knight(board, Color.WHITE);
+        Position knightPosition = new Position(4, 4);
+        board.placePiece(knight, knightPosition);
+
+        board.placePiece(new Rook(board, Color.BLACK), new Position(3, 2)); // 1 up, 2 left
+        board.placePiece(new Rook(board, Color.BLACK), new Position(2, 3)); // 2 up, 1 left
+        board.placePiece(new Rook(board, Color.BLACK), new Position(2, 5)); // 2 up, 1 right
+        board.placePiece(new Rook(board, Color.BLACK), new Position(3, 6)); // 1 up, 2 right
+        board.placePiece(new Rook(board, Color.BLACK), new Position(5, 6)); // 1 down, 2 right
+        board.placePiece(new Rook(board, Color.BLACK), new Position(6, 5)); // 2 down, 1 right
+        board.placePiece(new Rook(board, Color.BLACK), new Position(6, 3)); // 2 down, 1 left
+        board.placePiece(new Rook(board, Color.BLACK), new Position(5, 2)); // 1 down, 2 left
+
+        boolean[][] possibleMoves = knight.possibleMoves();
+
+        assertTrue(possibleMoves[3][2]); // 1 up, 2 left
+        assertTrue(possibleMoves[2][3]); // 2 up, 1 left
+        assertTrue(possibleMoves[2][5]); // 2 up, 1 right
+        assertTrue(possibleMoves[3][6]); // 1 up, 2 right
+        assertTrue(possibleMoves[5][6]); // 1 down, 2 right
+        assertTrue(possibleMoves[6][5]); // 2 down, 1 right
+        assertTrue(possibleMoves[6][3]); // 2 down, 1 left
+        assertTrue(possibleMoves[5][2]); // 1 down, 2 left
     }
 
     @Test
-    void increaseMoveCount() {
+    void testPossibleMoves_KnightAtEdgeOfBoard() {
+        Board board = new Board(8, 8);
+        Knight knight = new Knight(board, Color.WHITE);
+        Position knightPosition = new Position(0, 0);
+        board.placePiece(knight, knightPosition);
+
+        boolean[][] possibleMoves = knight.possibleMoves();
+
+        assertTrue(possibleMoves[1][2]); // 1 down, 2 right
+        assertTrue(possibleMoves[2][1]); // 2 down, 1 right
     }
 
     @Test
-    void decreaseMoveCount() {
-    }
+    void testPossibleMoves_KnightAtEdgeWithBlockedMoves() {
+        Board board = new Board(8, 8);
+        Knight knight = new Knight(board, Color.WHITE);
+        Position knightPosition = new Position(0, 0);
+        board.placePiece(knight, knightPosition);
 
-    @Test
-    void getChessPosition() {
-    }
+        board.placePiece(new Rook(board, Color.WHITE), new Position(1, 2)); // 1 down, 2 right
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1)); // 2 down, 1 right
 
-    @Test
-    void isThereOpponentPiece() {
-    }
+        boolean[][] possibleMoves = knight.possibleMoves();
 
-    @Test
-    void getBoard() {
-    }
-
-    @Test
-    void possibleMove() {
-    }
-
-    @Test
-    void isThereAnyPossibleMove() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
-    void possibleMoves() {
+        assertFalse(possibleMoves[1][2]); // 1 down, 2 right
+        assertFalse(possibleMoves[2][1]); // 2 down, 1 right
     }
 }
