0af42324a5305f582bb85677a42fd063c9f74ed3
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-29 21:57:27 -0300

    Project Chess Game - PawnTest implemented.

--- src/test/java/com/repliforce/chessgame/chess/pieces/PawnTest.java
+++ src/test/java/com/repliforce/chessgame/chess/pieces/PawnTest.java
@@ -1,60 +1,98 @@
 package com.repliforce.chessgame.chess.pieces;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-class PawnTest {
+import com.repliforce.chessgame.boardgame.Board;
+import com.repliforce.chessgame.boardgame.Position;
+import com.repliforce.chessgame.chess.Color;
+import com.repliforce.chessgame.chess.ChessMatch;
+
+public class PawnTest {
+
+    private Board board;
+    private ChessMatch chessMatch;
+    private Pawn bluePawn;
+    private Pawn redPawn;
 
     @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getColor() {
+    public void setUp() {
+        board = new Board(8, 8);
+        chessMatch = new ChessMatch();
+        bluePawn = new Pawn(board, Color.BLUE, chessMatch);
+        redPawn = new Pawn(board, Color.RED, chessMatch);
     }
 
     @Test
-    void getMoveCount() {
+    public void testBluePawnNormalMove() {
+        Position initialPosition = new Position(6, 3);
+        board.placePiece(bluePawn, initialPosition);
+
+        boolean[][] possibleMoves = bluePawn.possibleMoves();
+        assertTrue(possibleMoves[5][3], "Peão azul deve mover-se para frente uma casa.");
+        assertTrue(possibleMoves[4][3], "Peão azul deve mover-se duas casas à frente no primeiro movimento.");
     }
 
     @Test
-    void increaseMoveCount() {
+    public void testRedPawnNormalMove() {
+        Position initialPosition = new Position(1, 3);
+        board.placePiece(redPawn, initialPosition);
+
+        boolean[][] possibleMoves = redPawn.possibleMoves();
+        assertTrue(possibleMoves[2][3], "Peão vermelho deve mover-se para frente uma casa.");
+        assertTrue(possibleMoves[3][3], "Peão vermelho deve mover-se duas casas à frente no primeiro movimento.");
     }
 
     @Test
-    void decreaseMoveCount() {
+    public void testBluePawnCaptureOpponent() {
+        Position bluePawnPosition = new Position(4, 3);
+        board.placePiece(bluePawn, bluePawnPosition);
+
+        Position opponentPosition = new Position(3, 2);
+        Pawn opponentPawn = new Pawn(board, Color.RED, chessMatch);
+        board.placePiece(opponentPawn, opponentPosition);
+
+        boolean[][] possibleMoves = bluePawn.possibleMoves();
+        assertTrue(possibleMoves[3][2], "Peão azul deve poder capturar o peão adversário vermelho na diagonal.");
     }
 
     @Test
-    void getChessPosition() {
+    public void testRedPawnCaptureOpponent() {
+        Position redPawnPosition = new Position(3, 3);
+        board.placePiece(redPawn, redPawnPosition);
+
+        Position opponentPosition = new Position(4, 4);
+        Pawn opponentPawn = new Pawn(board, Color.BLUE, chessMatch);
+        board.placePiece(opponentPawn, opponentPosition);
+
+        boolean[][] possibleMoves = redPawn.possibleMoves();
+        assertTrue(possibleMoves[4][4], "Peão vermelho deve poder capturar o peão adversário azul na diagonal.");
     }
 
     @Test
-    void isThereOpponentPiece() {
+    public void testBluePawnCannotMoveToOccupiedPosition() {
+        Position bluePawnPosition = new Position(6, 3);
+        board.placePiece(bluePawn, bluePawnPosition);
+
+        Position blockingPosition = new Position(5, 3);
+        Pawn blockingPawn = new Pawn(board, Color.BLUE, chessMatch);
+        board.placePiece(blockingPawn, blockingPosition);
+
+        boolean[][] possibleMoves = bluePawn.possibleMoves();
+        assertFalse(possibleMoves[5][3], "Peão azul não deve poder mover-se para uma posição ocupada por outra peça azul.");
     }
 
     @Test
-    void getBoard() {
-    }
+    public void testRedPawnCannotMoveToOccupiedPosition() {
+        Position redPawnPosition = new Position(1, 3);
+        board.placePiece(redPawn, redPawnPosition);
 
-    @Test
-    void possibleMove() {
-    }
+        Position blockingPosition = new Position(2, 3);
+        Pawn blockingPawn = new Pawn(board, Color.RED, chessMatch);
+        board.placePiece(blockingPawn, blockingPosition);
 
-    @Test
-    void isThereAnyPossibleMove() {
-    }
-
-    @Test
-    void possibleMoves() {
-    }
-
-    @Test
-    void testToString() {
+        boolean[][] possibleMoves = redPawn.possibleMoves();
+        assertFalse(possibleMoves[2][3], "Peão vermelho não deve poder mover-se para uma posição ocupada por outra peça vermelha.");
     }
 }
